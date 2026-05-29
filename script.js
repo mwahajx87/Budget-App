b89febc529d49f050325cbac8aa52540c05e916d
Muhammad Wahaj <wahajwahaj200@gmail.com>
2026-05-29 09:53:19 +0000

    fix again

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ setBudget = () => {
     return;
   } else {
     totalBudget.innerText = totalAmount.value;
-    balance.innerText = totalAmount.value - parseFloat(expense.innerText);
+    balance.innerText = parseFloat(totalBudget.innerText) - parseFloat(expense.innerText);
     totalAmount.value = "";
     toastr.success("Budget set successfully!");
   }
